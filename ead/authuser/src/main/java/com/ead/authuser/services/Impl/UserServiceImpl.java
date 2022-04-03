9498620c4611987612ba8ea5dfd64e2844ee7c08
diego-freittas <tpddiego@gmail.com>
2022-04-03 19:52:17 -0300

    cria controller em authuser

--- ead/authuser/src/main/java/com/ead/authuser/services/Impl/UserServiceImpl.java
+++ ead/authuser/src/main/java/com/ead/authuser/services/Impl/UserServiceImpl.java
@@ -1,13 +1,34 @@
 package com.ead.authuser.services.Impl;
 
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Override
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userid) {
+        return userRepository.findById(userid);
+    }
+
+    @Override
+    public void deleteById(UUID userId) {
+        userRepository.deleteById(userId);
+    }
+
 }
