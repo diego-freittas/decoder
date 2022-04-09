cdc01c126f068d7880f74386aa77e5e3da4560bd
Diego <tpddiego@gmail.com>
2022-04-09 17:25:44 -0300

    Conclui implementacao do authuser

--- ead/authuser/src/main/java/com/ead/authuser/models/UserModel.java
+++ ead/authuser/src/main/java/com/ead/authuser/models/UserModel.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -13,10 +14,10 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
-@JsonInclude(JsonInclude.Include.ALWAYS)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Table(name = "TB_USERS")
-public class UserModel implements Serializable {
+public class UserModel extends RepresentationModel<UserModel>implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
