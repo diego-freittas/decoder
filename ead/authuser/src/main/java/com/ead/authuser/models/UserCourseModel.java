9744636599921e7395b6978cf0e33616b9172282
Diego <tpddiego@gmail.com>
2022-08-29 16:16:39 -0300

    produtos

---
+++ ead/authuser/src/main/java/com/ead/authuser/models/UserCourseModel.java
@@ -0,0 +1,40 @@
+package com.ead.authuser.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "TB_USERS_COURSES")
+public class UserCourseModel implements Serializable {
+    private static final long serialVersionUID =1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private UserModel user;
+
+    @Column(nullable = false)
+    private UUID courseId;
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
