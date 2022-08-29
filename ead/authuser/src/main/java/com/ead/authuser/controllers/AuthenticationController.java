1986641d4566fcb0e977e1843e548d26d149a4c7
Diego <tpddiego@gmail.com>
2022-08-29 16:16:57 -0300

    produtos

--- ead/authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ ead/authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -51,7 +51,7 @@ public class AuthenticationController {
         userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
         userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
         service.save(userModel);
-        log.debug("POST registerUser userModel saved {} ", userModel.toString());
+        log.debug("POST registerUser userId saved {} ", userModel.getUserId());
         log.info("User saved successfully userId {} ", userModel.getUserId());
         return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
     }
